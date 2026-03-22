b72dd7e23c6910caff7998f738a1229ce0a39468
Patrick Wayne Thomas <patrick@lazywolf.ltd>
2026-03-22 14:44:30 -0500

    Initial Commit

---
+++ EventCam/EventCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventCamApp.swift
+//  EventCam
+//
+//  Created by Patrick Thomas on 3/22/26.
+//
+
+import SwiftUI
+
+@main
+struct EventCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
